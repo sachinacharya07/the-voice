210f11cefeb7e802ec47af30a3de844bcb7972f1
sachinacharya07 <sachinbhattacharya12@gmail.com>
2026-06-02 10:41:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 800,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('firebase/auth')) return 'firebase-auth'
-          if (id.includes('firebase/firestore')) return 'firebase-firestore'
-          if (id.includes('firebase')) return 'firebase-core'
-          if (id.includes('react-dom')) return 'react-vendor'
-          if (id.includes('react-router')) return 'react-router'
-        }
-      }
-    }
+    chunkSizeWarningLimit: 1000
   }
 })
